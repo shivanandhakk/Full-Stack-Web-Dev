6e8138756af2422537c5901ce87f62c5e52d8488
SHIVANANDHA K K <kkshivanandha@gmail.com>
2025-12-15 20:57:02 +0530

    StateExample
    Added Inline and Internal and form structure to StateExample component.

--- StateExample.jsx
+++ StateExample.jsx
@@ -7,10 +7,25 @@ export default function StateExample() {
     }
     return (
         <>
-            <h1>State Example</h1>
-            <button onClick={buttonClick} style={{backgroundColor: "lightblue"}}>
+            <style>
+            {`
+            .box {
+                width: 380px;
+                padding: 70px;
+                text-align: center;
+                background-color: violet;
+                margin-left: 300px;
+            }
+            `}
+            </style>
+            
+            <form className="box">
+            <h1 style={{color: "brown", backgroundColor: "lightgreen", padding: "50px"}}>State Example</h1>
+            <button type="button" onClick={buttonClick} style={{backgroundColor: "lightblue", padding: "20px", fontSize: "20px"}}>
                 Visitor Count = {count}
             </button>
+            </form>
         </>
     );
+
 }
